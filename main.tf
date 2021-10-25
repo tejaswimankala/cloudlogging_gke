5fbd710735b60c8b7bc42e219ff944cb3284e7be
tejaswimankala <79300052+tejaswimankala@users.noreply.github.com>
2021-10-25 14:58:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  name                     = var.network_name
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = true
+}
+
+
+
+resource "google_container_cluster" "default" {
+  name               = var.network_name
+  location           = var.location
+  initial_node_count = 3
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = google_compute_subnetwork.default.name
+  subnetwork         = google_compute_subnetwork.default.name
+
+  
+  enable_legacy_abac = true
+
+  
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 90"
+  }
+}
