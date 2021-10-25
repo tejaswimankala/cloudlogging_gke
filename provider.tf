994b5cce2a520398752fae7dd6baeeaaba084c32
tejaswimankala <79300052+tejaswimankala@users.noreply.github.com>
2021-10-25 15:03:07 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,23 @@
+data "google_client_config" "current" {
+}
+
+data "google_container_engine_versions" "default" {
+  location = var.location
+}
+provider "kubernetes" {
+  version = "~> 1.10.0"
+  host    = google_container_cluster.default.endpoint
+  token   = data.google_client_config.current.access_token
+  client_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].client_certificate,
+  )
+  client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+provider "google" {
+  region = var.region
+  project = var.project
+}
