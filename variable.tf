2c25a7767aa3d417978207a4e0e78f4192a70524
tejaswimankala <79300052+tejaswimankala@users.noreply.github.com>
2021-10-25 15:04:13 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-west1"
+}
+
+variable "location" {
+  default = "us-west1-b"
+}
+
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
+
+variable "project" {
+  type        = string
+  
+}
